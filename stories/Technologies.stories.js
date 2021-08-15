6595adebf7d91957064932c1c5f86aa1128efbb7
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-08-15 11:55:23 -0400

    boilerplate

---
+++ stories/Technologies.stories.js
@@ -0,0 +1,13 @@
+import Technologies from "../technologies/Technologies";
+
+export default {
+  title: 'Screens/technologies',
+  component: Technologies
+};
+
+const Template = (args) => <Technologies {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+
+}
